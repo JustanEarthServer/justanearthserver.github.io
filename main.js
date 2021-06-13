0726919811ef234ff6233f7b3ffffee4d98d1fd4
Dmitry Pleshkov <dpleshkov.usa@gmail.com>
2021-06-12 18:47:51 -0700

    Initial Commit

---
+++ main.js
@@ -0,0 +1,20 @@
+const express = require("express");
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT;
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+app.get("/rules", (req, res) => {
+    res.render("rules");
+});
+
+app.listen(port, () => {
+    console.log(`jes.earth site listening on http://localhost:${port}`);
+});
