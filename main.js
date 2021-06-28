60c2b1a9dce90e3b73bd585896fb0a385bd28fff
Dmitry Pleshkov <dpleshkov.usa@gmail.com>
2021-06-28 00:54:51 -0800

    hotfix

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const express = require("express");
 require("dotenv").config();
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
